c45ab1a007fbb2b883c4486160ccae8b3737a53f
Muhammad Khezar <muhammadkhezar36@gmail.com>
2023-09-05 15:35:44 +0500

    changes

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-// export const base_url = "http://ec2-54-157-32-140.compute-1.amazonaws.com/api";
+export const base_url = "http://ec2-54-157-32-140.compute-1.amazonaws.com/api";
 
-export const base_url = "http://localhost:3006";
+// export const base_url = "http://localhost:3006";
